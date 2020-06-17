37f36e9050022610d9aa8564c71aacca1dce1c53
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2020-06-17 01:11:07 -0300

    slider theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -70,14 +70,24 @@ class _InputPageState extends State<InputPage> {
                       Text('cm', style: kLabelTextStyle),
                     ],
                   ),
-                  Slider(
-                    value: height.toDouble(),
-                    min: 100.0,
-                    max: 240.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (double value) =>
-                        setState(() => height = value.round()),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0xFFEB1555).withAlpha(0x29),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 100.0,
+                      max: 240.0,
+                      onChanged: (double value) =>
+                          setState(() => height = value.round()),
+                    ),
                   )
                 ],
               ),
